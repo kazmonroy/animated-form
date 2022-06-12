2f4f5a8d0c84fa41d2ed37548aa24498c274b88a
Katherine Monroy <kaz@Katherines-MacBook-Pro.local>
2022-06-12 14:00:24 -0500

    Add thanks message

--- app.js
+++ app.js
@@ -127,3 +127,30 @@ checkbox.addEventListener("click", () => {
     tl2.to(".checkbox-label", { color: "#adb5bd" }, "<");
   }
 });
+
+// Eyes animation
+
+gsap.set("#eye", { transformOrigin: "center" });
+gsap.fromTo(
+  "#eye",
+  { scaleY: 1 },
+  { scaleY: 0.3, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: "Power2.out" }
+);
+
+// Submit button
+const button = document.querySelector("button");
+
+const tl3 = gsap.timeline({
+  defaults: { duration: 0.75, ease: "Power3.easeOut" },
+});
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  tl3.to(".contact-left, .contact-right", {
+    y: 30,
+    opacity: 0,
+    pointerEvents: "none",
+  });
+  tl3.to("form", { scale: 0.8 }, "<");
+  tl3.fromTo(".submitted", { opacity: 0, y: 30 }, { opacity: 1, y: 0 }, "<50%");
+});
